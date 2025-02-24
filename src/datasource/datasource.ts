d7edb73a993a1258ddc6b40525b81f459d211a20
ManuelGonzalez <manuel_agh@hotmail.com>
2025-02-24 09:06:27 -0600

    datasource for typeorm-extension

--- src/datasource/datasource.ts
+++ src/datasource/datasource.ts
@@ -1,7 +1,7 @@
 import { DataSource } from 'typeorm';
 import { SeederOptions } from 'typeorm-extension';
-import configuration from '../config/configuration';
 import { MysqlConnectionOptions } from 'typeorm/driver/mysql/MysqlConnectionOptions';
+import configuration from '../config/configuration';
 
 const options: MysqlConnectionOptions & SeederOptions = {
   type: 'mysql',
